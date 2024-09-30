fb030abc58d08be90689eae623b6622d2d7383ac
フユカワ(Fuyukawa) <54656519+fuyukawasann@users.noreply.github.com>
2024-09-30 17:05:55 +0900

    Title: 날짜 변경하여 출력 2, Time: 94ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240930/날짜 변경하여 출력 2/print-date-with-different-format-2.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), "-");
+        
+        int m = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+
+        System.out.println(y+"."+m+"."+d);
+
+
+    }
+}
