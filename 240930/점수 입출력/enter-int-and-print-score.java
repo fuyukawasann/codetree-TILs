165faa23c1c8add3ff12c7683520d646b0cfdaaa
フユカワ(Fuyukawa) <54656519+fuyukawasann@users.noreply.github.com>
2024-09-30 16:38:07 +0900

    Title: 점수 입출력, Time: 105ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240930/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("Your score is ").append(n).append(" point.\n");
+
+        bw.write(sb.toString());
+        bw.flush();
+
+        
+    }
+}
