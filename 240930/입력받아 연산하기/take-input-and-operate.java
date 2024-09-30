993ca0f0bc988e4436f7b4a9fcca18bae65f3536
フユカワ(Fuyukawa) <54656519+fuyukawasann@users.noreply.github.com>
2024-09-30 23:49:49 +0900

    Title: 입력받아 연산하기, Time: 82ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240930/입력받아 연산하기/take-input-and-operate.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(Integer.parseInt(br.readLine()) + 87);
+        System.out.println(Integer.parseInt(br.readLine()) % 10);
+    }
+}
