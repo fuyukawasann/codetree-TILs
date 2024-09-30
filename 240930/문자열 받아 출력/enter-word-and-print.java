331cbd9da04f1d4d08fd5d534c3f2358c93558c1
フユカワ(Fuyukawa) <54656519+fuyukawasann@users.noreply.github.com>
2024-09-30 16:58:46 +0900

    Title: 문자열 받아 출력, Time: 94ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240930/문자열 받아 출력/enter-word-and-print.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(br.readLine());
+    }
+}
